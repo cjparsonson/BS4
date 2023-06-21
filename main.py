1704056c72de9244bca189c90a69ab0ba101438d
Chris Parsonson <cjparsonson@googlemail.com>
2023-06-21 19:55:51 +0100

    Build and send email message

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 # Imports
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
 import requests
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+# Load environment variables
+load_dotenv()
+
+
 
 # #  Open HTML file
 # with open('website.html', encoding='utf-8') as html_file:  # encoding='utf-8' is used to avoid UnicodeDecode errors
@@ -38,25 +48,55 @@ article_links = []
 article_scores = []
 
 for article in articles:
-    article_id = article.get("id")
-    article_class = article.find(class_="titleline")
-    article_text = article_class.find("a").getText()
-    article_link = article_class.find("a").get("href")
-    article_class_score = soup.find(class_="score", id=f"score_{article_id}").getText()
-    # append to lists
-    article_texts.append(article_text)
-    article_links.append(article_link)
-    article_scores.append(int(article_class_score.split()[0]))  # split the score on whitespace and convert to int
+    try:
+        article_id = article.get("id")
+        article_class = article.find(class_="titleline")
+        article_text = article_class.find("a").getText()
+        article_link = article_class.find("a").get("href")
+        article_class_score = soup.find(class_="score", id=f"score_{article_id}").getText()
+        # append to lists
+        article_texts.append(article_text)
+        article_links.append(article_link)
+        article_scores.append(int(article_class_score.split()[0]))  # split the score on whitespace and convert to int
+    except AttributeError:
+        pass
 
-#  get index of highest score
+#  get index of highest score and sorted list of scores
+scores_sorted = sorted(article_scores, reverse=True)
 highest_score = max(article_scores)
 highest_score_index = article_scores.index(highest_score)
 
-# print article with the highest score
-print(f"""Article with the highest score:
-{article_texts[highest_score_index]}
-{article_links[highest_score_index]}
-Upvotes: {article_scores[highest_score_index]}""")
+# Build message from top 5 articles
+email_message = ""
+for i in range(0, 5):
+    score_index = scores_sorted[i]
+    article_index = article_scores.index(score_index)
+    article_message = f"""{article_texts[article_index]}
+{article_links[article_index]}
+Upvotes: {article_scores[article_index]}
+
+"""
+    email_message += article_message
+
+# Email constants
+sender = os.getenv("sender")
+password = os.getenv("senderPwd")
+recipient = os.getenv("recipient")
+smtp_server = "smtp.gmail.com"
+smtp_port = 587
+message = EmailMessage()
+message.set_content(email_message)
+context = ssl.create_default_context()
+print(sender)
+print(password)
+
+# Send email
+with smtplib.SMTP(smtp_server, smtp_port) as connection:
+    connection.starttls(context=context)
+    connection.login(user=sender, password=password)
+    connection.sendmail(from_addr=sender, to_addrs=recipient, msg=f"Subject: Hacker News Top Stories\n\n{message}")
+
+
 
 
 
